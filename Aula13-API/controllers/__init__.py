14d1d543c15b0681faacde1f8e46cbcd724aa675
cezarToRres11 <12401650@aluno.cotemig.com.br>
2026-07-09 22:47:38 -0300

    Add files via upload

---
+++ Aula13-API/controllers/__init__.py
@@ -0,0 +1,5 @@
+from .api import api_v1_bp
+from .dashboard_controller import dashboard_bp
+from .figurinhas_controller import figurinhas_bp
+
+__all__ = ["dashboard_bp", "figurinhas_bp", "api_v1_bp"]
